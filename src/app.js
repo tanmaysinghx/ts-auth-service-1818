612961d4375b026ec7b268a10236a26e414b4329
Tanmay Singh <tanmaysinghx@gmail.com>
2024-07-21 13:33:53 +0530

    Removed index.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const { swaggerUi, swaggerSpec } = require('./config/swagger');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger'); // Adjust the import based on your setup
 
 dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
 // Serve Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use(bodyParser.json());
-
+// Routes
 const authRoutes = require('./routes/auth');
 const protectedRoutes = require('./routes/protected');
 const checkTokenRoute = require('./routes/checkToken');
-const refreshTokenRoute = require('./routes/refreshToken'); 
+const refreshTokenRoute = require('./routes/refreshToken');
 
 app.use('/auth', authRoutes); // Handles routes like /auth/register and /auth/login
 app.use('/protected', protectedRoutes); // Handles routes that need authentication
-
 app.use('/check-token', checkTokenRoute); // For /check-token endpoint
 app.use('/refresh-token', refreshTokenRoute); // For /refresh-token endpoint
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
